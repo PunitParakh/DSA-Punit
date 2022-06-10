2e96294dd4a68c1269c80d94d4f878078562b5a2
PunitParakh <85092944+PunitParakh@users.noreply.github.com>
2022-06-10 12:10:35 +0530

    Create 023. Merge two sorted list likedlist.cpp

---
+++ 023. Merge two sorted list likedlist.cpp
@@ -0,0 +1,36 @@
+Question - https://leetcode.com/problems/merge-two-sorted-lists/
+  
+Code - 
+  class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL)
+            return l2;
+        else if(l2 == NULL)
+            return l1;
+        
+        ListNode* result;
+        if(l1->val > l2->val)
+           swap(l1,l2);
+        
+        result = l1;
+        
+        ListNode* temp = NULL;
+        while(l1 != NULL && l2 != NULL)
+        {
+            if(l1->val <= l2->val){
+                temp = l1;
+                l1 = l1->next;
+            }
+            else{
+                temp->next = l2;
+                swap(l1,l2);
+            }
+        }
+        
+        if(l2)
+            temp->next = l2;
+
+        return result;
+    }
+};
